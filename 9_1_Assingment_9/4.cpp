3c9b9027fd7992fa970a297a145892c83f650b91
Debashis7307 <debashisalak@gmail.com>
2024-01-21 23:49:22 +0530

    a sample question of array

--- 9_1_Assingment_9/4.cpp
+++ 9_1_Assingment_9/4.cpp
@@ -1,5 +1,27 @@
-#include<iostream>
+// Recursive approach to check if an
+// Array is sorted or not
+#include <bits/stdc++.h>
 using namespace std;
+ 
+// Function that returns 0 if a pair
+// is found unsorted
+int arraySortedOrNot(int arr[], int n)
+{
+    // Array has one or no element or the
+    // rest are already checked and approved.
+    if (n == 1 || n == 0)
+        return 1;
+ 
+    // Unsorted pair found (Equal values allowed)
+    if (arr[n - 1] < arr[n - 2])
+        return 0;
+ 
+    // Last pair was sorted
+    // Keep on checking
+    return arraySortedOrNot(arr, n - 1);
+}
+ 
+// Driver code
 int main(){
     int x;
     cout<<"Enter the size of the array";
@@ -9,17 +31,9 @@ int main(){
     for(int i=0;i<x;i++){
         cin>>arr[i];
     }
-    bool check = false;
-    for(int i=1;i<=x;i++){
-        if(arr[i-1]<arr[i]){
-          check = true; 
-        }
-    }
-    if(check==true){
-        cout<<"Dammm! The given array is sorted";
-    }
-      else{
-            cout<<"The given array is not shorted ):";
-        }
+    if (arraySortedOrNot(arr, x))
+        cout << "Dammm! The given array is sorted\n";
+    else
+        cout << "No, The given array is not shorted ):\n";
     return 0;
 }
