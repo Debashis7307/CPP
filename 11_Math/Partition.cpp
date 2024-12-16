a38371d37777b4404c7a9c743654c2d416af4ebd
Debashis7307 <debashisalak@gmail.com>
2024-12-17 00:23:30 +0530

    aditional same type of question

--- 11_Math/Partition.cpp
+++ 11_Math/Partition.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"Enter the size:";
     cin>>n;
     vector<int> v(n);
-    cout<<"Enter the vector:";
+    cout<<"Enter the elements:";
     for(int i=0;i<n;i++){
         cin>>v[i];
        
@@ -16,9 +16,18 @@ int main(){
     for(int i=1;i<n;i++){
       run[i]=run[i-1]+v[i];
     }
-    cout<<"\n\tThe Running Sum of the vector is:";
+    int idx = -1;
     for(int i=0;i<n;i++){
-        cout<<" "<<run[i];
+        if(2*run[i]==run[n-1]){
+          idx = i;
+          break;
         }
+        }
+    if(idx!=-1){
+      cout<<"Yes ! The given list can be partitioned into two sub-lists, at the indx : "<<idx+1<<".\n";
+    }
+    else{
+        cout<<"No ! The given list cannot be partitioned into two sub-lists.\n";
+    }
     return 0;
 }
